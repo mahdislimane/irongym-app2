81ddc20b8259ee7d43f66f06e65052479adb1b3b
mahdi.slimen <mahdi.slimen@satoripop.com>
2021-01-12 12:26:13 +0100

    forth commit

--- connexion.js
+++ connexion.js
@@ -1,21 +1,21 @@
 const sql = require("mysql");
 
 const db = sql.createConnection({
-	user: "root", //default is sa
-	password: "",
-	server: "localhost", // for local machine
-	database: "irongymdb", // name of database
-	options: {
-		encrypt: true,
-		enableArithAbort: true,
-	},
+  user: "223697", //default is sa
+  password: "",
+  server: "mysql-mahdislimane.alwaysdata.net", // for local machine
+  database: "mahdislimane_irongymdb", // name of database
+  options: {
+    encrypt: true,
+    enableArithAbort: true,
+  },
 });
 db.connect(
-	// db,
-	// { useUnifiedTopology: true, useNewUrlParser: true },
-	(err) => {
-		if (err) throw err;
-		console.log("database connected....");
-	}
+  // db,
+  // { useUnifiedTopology: true, useNewUrlParser: true },
+  (err) => {
+    if (err) throw err;
+    console.log("database connected....");
+  }
 );
 module.exports = db;
