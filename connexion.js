d108da877eddef29f92226cbbf9c7bc0ea91eca1
mahdi.slimen <mahdi.slimen@satoripop.com>
2021-01-19 14:48:35 +0100

    today commit

--- connexion.js
+++ connexion.js
@@ -3,7 +3,7 @@ const sql = require("mysql");
 const db = sql.createConnection({
   user: "root", //default is sa
   password: "",
-  server: "localhost", // for local machine
+  server: "196.179.47.196:22068", // for local machine
   database: "irongymdb", // name of database
   options: {
     encrypt: true,
